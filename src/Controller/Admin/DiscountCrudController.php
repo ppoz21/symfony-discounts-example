0223df7cdb9d2eea0423405f55d7b31edfb3837c
Paweł Palacz <pawel.palacz@fabrity.pl>
2025-05-07 11:57:39 +0200

    added basic admin panel

--- src/Controller/Admin/DiscountCrudController.php
+++ src/Controller/Admin/DiscountCrudController.php
@@ -5,16 +5,22 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Entity\Discount;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminAction;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use RuntimeException;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 final class DiscountCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private readonly AdminUrlGenerator $adminUrlGenerator
+    ) {}
+
     public static function getEntityFqcn(): string
     {
         return Discount::class;
@@ -42,18 +48,18 @@ final class DiscountCrudController extends AbstractCrudController
         ;
     }
 
-    public function generateCodes(
-        AdminContext $adminContext
-    ): RedirectResponse {
+    #[AdminAction(routePath: '{entityId}/generate-codes', routeName: 'generate_codes')]
+    public function generateCodes(AdminContext $context): RedirectResponse
+    {
         $entityFqcn = self::getEntityFqcn();
 
         /** @var Discount $instance */
-        $instance = $adminContext->getEntity()->getInstance();
+        $instance = $context->getEntity()->getInstance();
 
         if (!$instance instanceof $entityFqcn) {
             throw new RuntimeException("Entity is not an instance of {$entityFqcn}");
         }
 
-        return $this->redirect($adminContext->getReferrer());
+        return $this->redirect($this->adminUrlGenerator->setAction(Action::INDEX)->generateUrl());
     }
 }
